ed71aa46df6a3779210b45694e5de214c62debaf
Bakaroo <bebojerk@gmail.com>
2026-07-25 18:38:07 +0300

    learning about different types of variables

---
+++ Js/let_const_var.js
@@ -0,0 +1,12 @@
+// different types of variables in JavaScript
+// 1. var
+var age = 25;
+console.log(age); // 25  
+
+// 2. let
+let city = "New York";
+console.log(city); // "New York"
+
+// 3. const
+const country = "USA";
+console.log(country); // "USA"  
