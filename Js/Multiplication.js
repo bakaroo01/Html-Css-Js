c3d005fa25941d2677d1793c9fdf5242f56d5772
Bakaroo <bebojerk@gmail.com>
2026-07-27 18:02:37 +0300

    learning about Arithmetic Operators and how to multiply in Java script

---
+++ Js/Multiplication.js
@@ -0,0 +1,8 @@
+const product = 10 * 5; // Multiplication
+console.log(product); // Output: 50
+
+// The second way to multiply two numbers.
+const num1 = 10;
+const num2 = 7;
+const result = num1 * num2;
+console.log("The product is: " + result); // Output: The product is: 70
