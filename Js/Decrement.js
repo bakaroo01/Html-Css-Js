2bb097538c9f3e872ad2480db3c18c5bd0033d34
Bakaroo <bebojerk@gmail.com>
2026-07-27 18:21:03 +0300

    learning about Arithmetic Operators and how to Decrement in Java script

---
+++ Js/Decrement.js
@@ -0,0 +1,13 @@
+// Two common ways to decrement a variable in JavaScript.
+
+// The first way to decrement a variable is to use the decrement operator (--).
+
+let myVar = 5;
+myVar--;
+console.log(myVar); // Output: 4
+
+// The second way is to use the subtraction assignment operator (-=).
+
+let myVar2 = 5;
+myVar2 -= 1;
+console.log(myVar2); // Output: 4
