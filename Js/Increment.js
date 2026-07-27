c95801c2e7864cb08b28da3dcbb341d9bad2d341
Bakaroo <bebojerk@gmail.com>
2026-07-27 18:16:35 +0300

    learning about Arithmetic Operators and how to Increment in Java script

---
+++ Js/Increment.js
@@ -0,0 +1,11 @@
+// Two common ways to increment a variable in JavaScript.
+
+// The first way is to use the increment operator (++).
+let myVar = 5;
+myVar++;
+console.log(myVar); // Output: 6
+
+// The second way is to use the addition assignment operator (+=).
+let myVar2 = 5;
+myVar2 += 1;
+console.log(myVar2); // Output: 6
