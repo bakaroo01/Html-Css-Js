bd273493954197a91856171172002c4c762a8717
Bakaroo <bebojerk@gmail.com>
2026-07-27 17:50:23 +0300

    learning about Arithmetic Operators and how to subtract in Java script

---
+++ Js/Subtract.js
@@ -0,0 +1,10 @@
+// The first way to subtract two numbers.
+const sub = 10 - 5; // Subtraction
+console.log(sub); // Output: 5
+
+
+// The second way to subtract two numbers.
+const num1 = 10;
+const num2 = 7;
+const result = num1 - num2;
+console.log("The difference is: " + result); // Output: The difference is: 3
