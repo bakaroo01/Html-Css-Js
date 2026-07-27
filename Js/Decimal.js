062332ad3517c3003b98b7f4f6b3b63673f092a0
Bakaroo <bebojerk@gmail.com>
2026-07-27 18:43:03 +0300

    learning about Arithmetic Operators and how to use Decimal numbers in Java script

---
+++ Js/Decimal.js
@@ -0,0 +1,14 @@
+//Arithmetic operators using decimal numbers in JavaScript
+
+
+let ourDecimal = 5.4;
+console.log(ourDecimal); // Output: 5.4
+
+// Multiplying two decimal numbers.
+const product = 2.5 * 2.0; 
+console.log(product); // Output: 5
+
+
+//  Dividing two decimal numbers.
+const quotient = 4.4 / 2.0; 
+console.log(quotient); // Output: 2.2 
