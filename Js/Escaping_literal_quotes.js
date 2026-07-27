0089dd06b0a88f50b95191bb5405dadfa1d57ed9
Bakaroo <bebojerk@gmail.com>
2026-07-27 22:27:46 +0300

    learning about escaping literal quotes using single and double quotes

---
+++ Js/Escaping_literal_quotes.js
@@ -0,0 +1,14 @@
+// Escaping literal quotes in strings
+
+
+const myStr = "Hello, \"My name is Ahmed\"" ;
+
+const myStr2 = "Hi, \"My name is Mona\"";
+
+const myStr3 = 'Hello, \'My name is Bakar\'';
+
+const myStr4 = 'Hi, \'My name is Sara\'';
+console.log(myStr);
+console.log(myStr2);
+console.log(myStr3);
+console.log(myStr4);
