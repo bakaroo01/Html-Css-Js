a4ea12e572edd2cc4b42d1ee03cb335ffbe76062
Bakaroo <bebojerk@gmail.com>
2026-07-27 17:44:17 +0300

    learning about Arithmetic Operators and how to add in Java script

---
+++ Js/adding.js
@@ -0,0 +1,13 @@
+// Arithmatic Operators
+// The first way to add two numbers.
+
+const sum = 10 + 5; // Addition
+console.log(sum);// Output: 15
+
+// The second way to add two numbers.
+
+
+const num1 = 10;
+const num2 = 7;
+const result = num1 + num2; // Addition
+console.log("The sum is: " + result); // Output: The sum is: 17
