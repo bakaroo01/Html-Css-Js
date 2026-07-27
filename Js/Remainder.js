c4a7c45bddc0270d014e9a1f05df8623236c4d95
Bakaroo <bebojerk@gmail.com>
2026-07-27 18:46:59 +0300

    learning about Arithmetic Operators and Remainders in Java script

---
+++ Js/Remainder.js
@@ -0,0 +1,10 @@
+// The two common ways to find the remainder of two numbers.
+
+let remainder = 10 % 3;
+console.log(remainder); // Output: 1
+
+// The second way to find the remainder of two numbers.
+const num1 = 10;
+const num2 = 3;
+const result = num1 % num2;
+console.log("The remainder is: " + result); // Output: The remainder is: 1
