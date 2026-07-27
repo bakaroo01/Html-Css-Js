1e4cfd35060ff84f09d605933aa26d737459a014
Bakaroo <bebojerk@gmail.com>
2026-07-27 18:05:14 +0300

    learning about Arithmetic Operators and how to divide in Java script

---
+++ Js/Divide.js
@@ -0,0 +1,8 @@
+const quotient = 10 / 5; // Division
+console.log(quotient); // Output: 2
+
+// The second way to divide two numbers.
+const num1 = 10;
+const num2 = 2;
+const result = num1 / num2;
+console.log("The quotient is: " + result); // Output: The quotient is: 5
