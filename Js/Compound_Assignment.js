c014960c7e5228c9756be422e569643c111a357b
Bakaroo <bebojerk@gmail.com>
2026-07-27 18:52:18 +0300

    learning about Arithmetic Operators and Compound Assignment in Java script

---
+++ Js/Compound_Assignment.js
@@ -0,0 +1,17 @@
+// Compound Assignment Operators in JavaScript
+
+let a = 5;
+a += 2; // Equivalent to a = a + 2
+console.log(a); // Output: 7
+
+let b = 10;
+b -= 4; // Equivalent to b = b - 4
+console.log(b); // Output: 6
+
+let c = 4;
+c *= 2; // Equivalent to c = c * 2
+console.log(c); // Output: 8
+
+let d = 12;
+d /= 3; // Equivalent to d = d / 3
+console.log(d); // Output: 4
