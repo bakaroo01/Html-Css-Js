85b6e6a66a2762327a723807b1f70c16ba5d4e66
Bakaroo <bebojerk@gmail.com>
2026-07-28 13:31:56 +0300

    learning about Bracket notation nth character or in other words any character you want to acces or retrieve using their Index

---
+++ Js/Bracket_notation_nth_char.js
@@ -0,0 +1,11 @@
+// Bracket notation nth character
+
+// In JavaScript, you can access individual characters of a string using bracket notation 
+
+// and the index of the character you want to retrieve.
+
+// Example of accessing the nth character of a string
+
+const myString = "Hello, World!";
+const nthCharacter = myString[7];
+console.log(nthCharacter); // Output: "W"   
