b3baf6df56d966907f4fe10451b4e99955ee27f7
Bakaroo <bebojerk@gmail.com>
2026-07-28 12:17:17 +0300

    learning about length of string

---
+++ Js/length_of_string.js
@@ -0,0 +1,13 @@
+// Length of a string not like index of a string,
+
+//  length is a property of a string. 
+
+// It is used to find the length of a string.
+
+// and it starts counting from 1 not from 0 like index of a string.
+
+
+let lastNameLength = 0;
+const lastName = "Lovelace";
+lastNameLength = lastName.length;   
+console.log(lastNameLength);
