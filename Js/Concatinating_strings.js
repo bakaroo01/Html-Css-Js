e2891c16ef5945ec5d61979dfc98d80190adc544
Bakaroo <bebojerk@gmail.com>
2026-07-28 11:24:37 +0300

    learning about Concatinating strings

---
+++ Js/Concatinating_strings.js
@@ -0,0 +1,10 @@
+// Concatenating strings with the + operator
+
+const myStr = "This is the first sentence. " + "This is the second sentence.";
+console.log(myStr);
+
+// Concatenating strings with the += operator
+
+let myStr2 = "This is the first part. ";
+myStr2 += "This is the second part.";
+console.log(myStr2);
