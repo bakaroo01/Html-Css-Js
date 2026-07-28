5f53bfc9a8898dfa1f6b34dcf58a653807a11181
Bakaroo <bebojerk@gmail.com>
2026-07-28 13:44:06 +0300

    learning about Bracket notation last character

---
+++ Js/Bracket_notation_last_char.js
@@ -0,0 +1,13 @@
+// Bracket notation last character
+
+// In JavaScript, you can access the last character of a string using bracket notation
+
+// and the index of the last character, which is the length of the string minus one.
+
+// Example of accessing the last character of a string
+
+const myString = "Hello, World!";
+const lastCharacter = myString[myString.length - 1];
+console.log(lastCharacter); // Output: "!"
+
+
