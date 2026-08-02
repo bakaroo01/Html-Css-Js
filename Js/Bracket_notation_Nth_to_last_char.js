dbdfdc8374e7a5ec34cf537003e6cf0fd372b35b
Bakaroo <bebojerk@gmail.com>
2026-08-02 20:17:17 +0300

    learning about Bracket notation Nth to last character in other words any number or letter before the last

---
+++ Js/Bracket_notation_Nth_to_last_char.js
@@ -0,0 +1,12 @@
+// Bracket notation Nth to last character
+
+// In JavaScript, you can access the Nth to last character of a string using bracket notation
+
+// and the index of the Nth to last character, which is the length of the string minus N.
+
+let lastName ="Lovelace"
+let secondToLast = lastName[lastName.length - 2]
+console.log(secondToLast) // Output: "c"
+
+let thirdToLast = lastName[lastName.length - 3]
+console.log(thirdToLast) // Output: "a"
