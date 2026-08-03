9b16badfd0c5bc6e7b9197b7b2d66eadc6032e10
Bakaroo <bebojerk@gmail.com>
2026-08-03 21:32:39 +0300

    learning about Nested Arrays

---
+++ Js/Nested_Arrays.js
@@ -0,0 +1,17 @@
+// Nested Arrays
+
+// A nested array is an array that contains other arrays as its elements. 
+
+// You can access the elements of a nested array using multiple indices.    
+
+
+let nestedArray = [
+  [1, 2, 3],
+  [4, 5, 6],
+  [7, 8, 9]
+];
+
+
+console.log(nestedArray[0][0]); // Output: 1
+console.log(nestedArray[1][2]); // Output: 6
+console.log(nestedArray[2][1]); // Output: 8
