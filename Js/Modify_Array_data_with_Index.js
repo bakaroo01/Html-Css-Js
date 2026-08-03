8aa44fcf82ef3c136b8b5a0f6e64e4ad43bcb882
Bakaroo <bebojerk@gmail.com>
2026-08-03 21:45:30 +0300

    learning about Modifying arrays data with their Indexes

---
+++ Js/Modify_Array_data_with_Index.js
@@ -0,0 +1,17 @@
+// Modifying array data with index 
+
+// You can modify the values in an array by accessing them 
+
+// using their index and assigning a new value.
+
+let numbers = [10, 20, 30, 40, 50];
+
+console.log(numbers); // Output: [10, 20, 30, 40, 50]
+
+// Modifying the value at index 2
+numbers[2] = 65;
+console.log(numbers); // Output: [10, 20, 65, 40, 50]
+
+// Arrays are mutable, which means you can change their contents without creating a new array.
+
+// unlike strings, which are immutable, arrays can be modified in place.
