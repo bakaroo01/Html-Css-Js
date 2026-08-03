a66e1d04b3175d9d89920e0767d60e1e73efeda0
Bakaroo <bebojerk@gmail.com>
2026-08-03 23:10:34 +0300

    learning about Manipulating an array with the unshift method

---
+++ Js/Manipulate_Array_with_unshift.js
@@ -0,0 +1,19 @@
+// Manipulating an array with the unshift method in JavaScript
+
+let myArray = [2, 3, 4, 5];
+myArray.unshift(1);
+console.log(myArray); // Output: [1, 2, 3, 4, 5]    
+
+// You can also unshift multiple elements by passing them as arguments to the unshift method
+
+myArray.unshift(-1, 0);
+console.log(myArray); // Output: [-1, 0, 1, 2, 3, 4, 5]
+
+// unshift method adds one or more elements to the beginning of an array
+//  and returns the new length of the array.
+
+
+
+
+
+
