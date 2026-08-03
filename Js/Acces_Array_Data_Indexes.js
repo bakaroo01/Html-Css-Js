d045598dfba8f251a27a50442312a8ac5da129af
Bakaroo <bebojerk@gmail.com>
2026-08-03 21:37:52 +0300

    learning about Accesing Arrays using their Indexes

---
+++ Js/Acces_Array_Data_Indexes.js
@@ -0,0 +1,16 @@
+// Accessing Array Data Using Indexes
+
+let fruits = ["apple", "banana", "cherry", "date"];
+
+console.log(fruits); // Output: ["apple", "banana", "cherry", "date"]
+
+console.log(fruits[0]); // Output: "apple"
+
+
+console.log(fruits[1]); // Output: "banana"
+
+
+console.log(fruits[2]); // Output: "cherry"
+
+
+console.log(fruits[3]); // Output: "date"
