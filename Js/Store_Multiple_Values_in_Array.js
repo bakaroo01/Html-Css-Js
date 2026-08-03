045ccc712fd672e59068b0dadea3209a4194f03f
Bakaroo <bebojerk@gmail.com>
2026-08-03 21:23:59 +0300

    learning about storing Multiple Values using Arrays

---
+++ Js/Store_Multiple_Values_in_Array.js
@@ -0,0 +1,16 @@
+// Storing Multiple Values in an Array
+
+
+let values = [1, 2, 3, 4, 5];
+console.log(values);// Output: [1, 2, 3, 4, 5]
+
+
+
+// You can also store different types of values in an array
+let mixedValues = [1, "two", true, null, { key: "value" }];
+console.log(mixedValues); // Output: [1, "two", true, null, { key: "value" }]
+
+// You can access values in an array using their index
+console.log(values[0]); // Output: 1
+console.log(values[2]); // Output: 3
+console.log(mixedValues[1]); // Output: "two"
