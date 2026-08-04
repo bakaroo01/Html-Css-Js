532049e55507e4b0fc4467431f720bc431ba9ce6
Bakaroo <bebojerk@gmail.com>
2026-08-04 23:32:13 +0300

    learning about Retuned value from a Function

---
+++ Js/Returned_Value_Assignment.js
@@ -0,0 +1,15 @@
+// Returned Value 
+
+
+// here's an example of a function that returns a value:
+
+function functionWithReturn() {
+    var localVariable = "I am a local variable.";
+    return localVariable; // Returning the local variable
+}
+var resultWithReturn = functionWithReturn();
+console.log(resultWithReturn); // This will log "I am a local variable."
+
+// In summary, if a function does not have a return statement, it will return undefined.
+
+//  If it has a return statement, it will return the specified value.
