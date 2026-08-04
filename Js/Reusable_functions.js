300c51104f8fe2a2d5fd1b8dbe97a962694c7dad
Bakaroo <bebojerk@gmail.com>
2026-08-04 21:41:24 +0300

    learning about function and how to use it

---
+++ Js/Reusable_functions.js
@@ -0,0 +1,17 @@
+// Reusable Functions
+
+function reusableFunction() {
+    console.log("This is a reusable function.");
+
+
+}
+    reusableFunction(); // Calling the function to demonstrate its usage
+
+
+    // Another example of a reusable function
+    function greeting() {
+        console.log("Hi Everyone! Welcome to the reusable functions demo.");
+
+
+    }
+    greeting(); // Calling the function to demonstrate its usage
