8d94ffe642fb47782c6518bd61e0b6d980710545
Bakaroo <bebojerk@gmail.com>
2026-08-04 22:36:30 +0300

    learning about Local Scope And functiosn and how they work

---
+++ Js/Local_scope_and_Functions.js
@@ -0,0 +1,16 @@
+// Local Scope and Functions
+
+function localScopeFunction() {
+    var localVariable = "I am a local variable.";
+    console.log(localVariable); // Accessing the local variable inside the function
+}
+
+localScopeFunction(); // Calling the function to demonstrate local scope
+
+// Trying to access the local variable outside the function will result in an error
+
+
+// console.log(localVariable); // Uncommenting this line will throw a ReferenceError
+
+
+
