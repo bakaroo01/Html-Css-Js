f31c7d63ef2ebc7e5b047dac16e47b68cd1ecc96
Bakaroo <bebojerk@gmail.com>
2026-08-04 22:04:44 +0300

    learning about Returning Values from a Function using return statement

---
+++ Js/Return_Value_function_with_Return.js
@@ -0,0 +1,15 @@
+// Returning Value from a function using return statement
+
+function add(a, b) {
+    return a + b; // Return the sum of a and b
+}
+
+console.log("The sum of 5 and 10 is: " + add(5, 10)); // Calling the function and logging the returned value
+
+
+// Another example of a function that returns a value
+function multiply(x, y) {
+    return x * y; // Return the product of x and y
+}
+
+console.log("The product of 4 and 6 is: " + multiply(4, 6)); // Calling the function and logging the returned value
