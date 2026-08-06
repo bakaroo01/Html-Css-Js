c5aafd4352250994e499e813c61639e49b098c58
Bakaroo <bebojerk@gmail.com>
2026-08-06 15:18:57 +0300

    Modifying

--- Js/Returned_Value_from_a_function.js
+++ Js/Returned_Value_from_a_function.js
@@ -10,6 +10,16 @@ function functionWithReturn() {
 var resultWithReturn = functionWithReturn();
 console.log(resultWithReturn); // This will log "I am a local variable."
 
+
+//here's another example of a function that  doesn't returns a value:
+
+function functionWithoutReturn() {
+    var localVariable = "I am a local variable.";
+    console.log(localVariable);
+}
+var result = functionWithoutReturn();
+console.log(result); // This will log undefined
+
 // In summary, if a function does not have a return statement, it will return undefined.
 
 //  If it has a return statement, it will return the specified value.
