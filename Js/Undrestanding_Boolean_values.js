b57059a7f90a58fdd41acb93684cb9c2c438dec9
Bakaroo <bebojerk@gmail.com>
2026-08-06 15:43:59 +0300

    Learning about Boolean Values and Understanding how they work in JS

---
+++ Js/Undrestanding_Boolean_values.js
@@ -0,0 +1,17 @@
+// Understanding Boolean Values in JavaScript
+
+// Boolean values are either true or false
+var isTrue = true;
+var isFalse = false;
+
+console.log(isTrue);  // This will log true
+console.log(isFalse); // This will log false
+
+// Boolean values are often used in conditional statements
+if (isTrue) {
+    console.log("The value is true");
+} else {
+    console.log("The value is false");
+}
+
+
