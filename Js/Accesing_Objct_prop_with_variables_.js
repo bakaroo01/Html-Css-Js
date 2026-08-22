86d41b8b1d36b01262b0fcdf9747f71219004691
Bakaroo <bebojerk@gmail.com>
2026-08-22 17:46:37 +0300

    Learning about how to Access Object properties with Variables

---
+++ Js/Accesing_Objct_prop_with_variables_.js
@@ -0,0 +1,9 @@
+// Accesing Object Properties with variables
+
+const testObj = {
+    12: "Namath",
+    16: "Montana",
+    19: "Unitas",
+};
+const playerNumber = 16;
+const player = testObj[playerNumber];
