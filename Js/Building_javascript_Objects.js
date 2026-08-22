2cca31892504bb49fa117f7741e59cbd79eebb3b
Bakaroo <bebojerk@gmail.com>
2026-08-22 16:58:30 +0300

    Learning about Objects

---
+++ Js/Building_javascript_Objects.js
@@ -0,0 +1,12 @@
+// Building JavaScript Objects
+
+const myDog = {
+    name : "Huskey",
+    legs : 4,
+    tails: 1,
+    friends : ['dog a' , 'dog b']
+
+}
+
+
+// you can build array inside an object 
