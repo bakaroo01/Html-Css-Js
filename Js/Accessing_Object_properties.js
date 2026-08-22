7d3c6fb55fa1970b0a99e14845f749106190eaaf
Bakaroo <bebojerk@gmail.com>
2026-08-22 17:15:54 +0300

    Learning about how to Access Object properties with Dot notations

---
+++ Js/Accessing_Object_properties.js
@@ -0,0 +1,13 @@
+// Accessing Object properties with Dot notations
+
+const testObj = {
+    hat : "ballcap",
+    shirt : "Jersey",
+    shoes : "cleats",
+};
+
+const hatValue = testObj.hat;
+const shirtValue = testObj.shirt;
+
+console.log(testObj)// this will log the whole object
+console.log(testObj.hat)// this will log hat's value
