ecd85b34e1917d550bfdcc9c77206ffb0e877d5d
Bakaroo <bebojerk@gmail.com>
2026-08-22 17:35:11 +0300

    Learning about how to Access Object properties with Bracket notations

---
+++ Js/Accessing_objct_propert_with_Bracket.js
@@ -0,0 +1,14 @@
+// Accesing Object properties with Bracket notations
+
+const testObj = {
+    "an entree": "hamburger",
+    "my side": "veggies",
+    "the drink":"water",
+};
+
+const entreeValue = testObj["an entree"]
+const drinkValue = testObj["the drink"]
+
+// you can't access with the dot notation since ther is aspace in the key properte
+
+// so that's why you use Bracket notation to access them
