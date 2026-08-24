203e9584f3ac0f57a90cd2bb27e87683a604b19e
Bakaroo <bebojerk@gmail.com>
2026-08-24 14:17:47 +0300

    Learning about adding properties after initiliazing objects

---
+++ Js/Adding_new_prop_to_Object.js
@@ -0,0 +1,16 @@
+// Adding new properties to Objects
+
+const myDog = {
+    name : "Coder",
+    legs : 4,
+    tails : 1,
+    friends : ["freeCodeCamp Campers"],
+};
+
+myDog.bark = "bow-wow";
+
+console.log(myDog)
+
+
+
+
