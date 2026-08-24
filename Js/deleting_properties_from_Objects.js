6815505c86ca03ce098b5fffd45c2bcdc7b69c26
Bakaroo <bebojerk@gmail.com>
2026-08-24 14:22:46 +0300

    Learning about deleting properties after intializing

---
+++ Js/deleting_properties_from_Objects.js
@@ -0,0 +1,13 @@
+const myDog = {
+    name : "Coder",
+    legs : 4,
+    tails : 1,
+    friends : ["freeCodeCamp Campers"],
+    bark:"bow-wow",
+};
+
+
+delete myDog.tails
+
+
+console.log(myDog)
