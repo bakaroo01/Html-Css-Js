23c49a4539faf6c04e570c4730fa1ba403b35691
Bakaroo <bebojerk@gmail.com>
2026-08-29 19:02:25 +0300

    Learning about Iterarting Throug an Array with a For Loop

---
+++ Js/Iterate_Throgh_Array_with_For_Loop.js
@@ -0,0 +1,14 @@
+// Iterating Through an Array with a For Loop
+
+
+const myArray = [2, 3, 4, 5, 6];
+
+let total = 0;
+
+for (let index = 0; index < myArray.length; index++){
+    total += myArray[index]
+    
+
+}
+
+console.log(total) 
